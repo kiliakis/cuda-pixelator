06244209b9ab94a13f5b3140f4dfbfabed9877b8
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2022-04-18 10:07:15 +0200

    adds run script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for v in CPU OPENMP CUDA; do
-    for size in 256x256 1024x1024 2048x2048 4096x4096; do
+    for size in 256x256.png 1024x1024.png 2048x2048.png 4096x4096.jpg; do
         echo $v $size;
-        ./bin/release/AdaptiveHistogramOptimization $V samples/$size.png -b &> results/$v-$size.txt;
+        ./bin/release/AdaptiveHistogramOptimisation $v samples/$size -b > results/$v-$size.txt;
     done
 done
 
